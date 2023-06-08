0f7bbabc9389f572f7f20837d042bde6b8bf8545
Erwin Fabro <erwinfabro@gmail.com>
2023-06-08 09:24:43 -0300

    se agrega titulo a la imagen

--- main.js
+++ main.js
@@ -4,19 +4,19 @@
 
 
 // script.js
-var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg',
+let images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg',
   '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg',
   '11.jpg', '12.jpg', '13.jpg', '14.jpg',
   '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg'];
 
-var container = document.getElementById("gallery");
+  let container = document.getElementById("gallery");
 for (let i = 0; i < 5; i++) {
-  for (var image of images) {
-    var img = document.createElement("img");
+  for (let image of images) {
+    let img = document.createElement("img");
     img.src = image;
     container.appendChild(img);
 
-    var imageName = document.createElement("p");
+    let imageName = document.createElement("p");
     imageName.textContent = image;
     container.appendChild(imageName);
   }
