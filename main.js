c4ca67eb16d0babc4a1bf17a18b7c4a0d81094a0
Erwin Fabro <119829191+ErwinFabro@users.noreply.github.com>
2023-06-07 23:06:16 -0300

    se agregan mas imagenes

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var images = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg'];
+var images = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','13.jpg','14.jpg','15.jpg','16.jpg','17.jpg','18.jpg','19.jpg'];
 var container = document.getElementById("gallery");
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 5; i++) {
   for (var image of images) {
     var img = document.createElement("img");
     img.src = image;
